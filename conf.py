9c440c5d1490265e643a72f7b7a27fc952709b8f
Herman Sheremetyev <herman@swebpage.com>
2011-11-27 23:33:29 +0900

    Actually updating the version

--- conf.py
+++ conf.py
@@ -48,7 +48,7 @@ copyright = u'2011, Herman Sheremetyev'
 # built documents.
 #
 # The short X.Y version.
-version = '0.9.0'
+version = '0.9.1'
 # The full version, including alpha/beta/rc tags.
 release = '0.9.0'
 
