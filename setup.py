5f355e6fc379542b2493ad270d3f7da43398a731
Herman Sheremetyev <herman@swebpage.com>
2012-04-14 01:38:09 +0900

    Adding MANIFEST.in with all relevant files

--- setup.py
+++ setup.py
@@ -3,4 +3,8 @@ from distutils.core import setup
 setup(name='flexmock',
       version='0.9.3',
       py_modules=['flexmock'],
+      author='Herman Sheremetyev',
+      author_email='herman@swebpage.com',
+      url='http://has207.github.com/flexmock/',
+      license='FreeBSD style license',
 )
