5fe7e817d33a47624d83cec7c095ee365d389e7c
Herman Sheremetyev <herman@swebpage.com>
2013-03-30 13:20:24 +0900

    Update to 0.9.7

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='flexmock',
-      version='0.9.6',
+      version='0.9.7',
       py_modules=['flexmock'],
       author='Herman Sheremetyev',
       author_email='herman@swebpage.com',
